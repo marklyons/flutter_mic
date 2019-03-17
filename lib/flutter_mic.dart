ae53c667b1c0ff1e67d533943e79fa4ba0537e15
Mark Lyons <lyons340@gmail.com>
2019-03-17 15:50:57 -0400

    Connecting function in Dart

--- lib/flutter_mic.dart
+++ lib/flutter_mic.dart
@@ -10,4 +10,9 @@ class FlutterMic {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  void startRecording {
+    print("Recording is being started.");
+    _channel.invokeMethod("startRecording");
+  }
 }
